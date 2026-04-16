cb2f81c0101317f9f143515e14800cb65aaef545
Margarita <gorbashevarira@gmail.com>
2026-04-16 19:49:35 +0300

    Added input validation and result display

--- converter/converter.js
+++ converter/converter.js
@@ -22,8 +22,32 @@ function convertCurrency(amount, fromCurrency, toCurrency) {
 
 button_green.addEventListener('click', function() {
   const amount = Number(input.value);
+
+  if(!input.value) {
+    alert('===    Пожалуйста, введите СУММУ для конвертации!    ===');
+    return;
+  }
+  if(currency_input.value === '') {
+    alert('===    Пожалуйста, введите ВАЛЮТУ ИЗ которой КОНВЕРТИРУЕМ!    ===');
+    return;
+  }
+  if (currency_output.value === '') {
+    alert('===    Пожалуйста, введите ВАЛЮТУ, В которую КОНВЕРТИРУЕМ!    ===');
+    return;
+  }
+
   const fromCurrency = currency_input.value;
   const toCurrency = currency_output.value;
   const convertedAmount = convertCurrency(amount, fromCurrency, toCurrency);
   output.value = convertedAmount;
+  output.style.backgroundColor = '#65ca87';
 });
+
+button_red.addEventListener('click', function() {
+    input.value = '',
+    currency_input.value = '',
+    currency_output.value = '', 
+    output.value = ''
+    output.style.backgroundColor = '#faebd7';
+}); 
+
